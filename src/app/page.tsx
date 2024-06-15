cf91e7043ead63b67e2c3ab6ec8d303ca95f011a
Apurva Kulkarni <apurvakulkarni098@gmail.com>
2024-06-15 23:45:36 +0530

    Database setup added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/a990e9e8-c144-4b2b-8a19-758bdeafefdd-yqspls.jpg",
@@ -14,19 +15,28 @@ const mockImages = mockUrls.map((url, index)=>({
   id: index + 1,
   url
 }))
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+  console.log("post:", posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
         {
-          [...mockImages, ...mockImages, ...mockImages].map((image)=>(
-            <div key={image.id} className="w-48">
+          posts.map((post)=>(
+            <div key={post.id}>
+              {post.name}
+            </div>
+          ))
+        }
+        {
+          [...mockImages, ...mockImages, ...mockImages].map((image, index)=>(
+            <div key={image.id + '_' + index} className="w-48">
               <img src={image.url} />
             </div>
           ))
         }
       </div>
-     Hello gallery in progress
     </main>
   );
 }
