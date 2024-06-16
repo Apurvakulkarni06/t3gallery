60fb222813a4d0193d6f130d985143edf5fb38c1
Apurva Kulkarni <apurvakulkarni098@gmail.com>
2024-06-16 18:09:48 +0530

    used next image to render images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,38 @@
 import { db } from "~/server/db";
-import { SignInButton, SignedIn, SignedOut, UserButton} from '@clerk/nextjs';
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { getImages } from "~/server/queries";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
-async function Images(){
-  
+async function Images() {
   const images = await getImages();
 
-  return(
-    <div className="flex flex-wrap gap-4">
-    {
-      images.map((image, index)=>(
-        <div key={image.id + '_' + index} className="flex w-48 flex-col">
-          <img className="h-48 object-cover" src={image.url} />
+  return (
+    <div className="flex flex-wrap justify-center gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex h-48 w-48 flex-col">
+          <Image
+            src={image.url}
+            alt={image.name}
+            style={{ objectFit: "contain" }}
+            width={192}
+            height={192}
+          />
           <p>{image.name}</p>
         </div>
-      ))
-    }
+      ))}
     </div>
-  )
+  );
 }
 
 export default async function HomePage() {
-
-  
   return (
     <main className="">
       <SignedOut>
-        <div className="w-full h-full text-2xl text-center">Please sign in from above</div>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in from above
+        </div>
       </SignedOut>
       <SignedIn>
         <Images />
