5d54a9c6e9f4b3648688e48c58e938d05aaef0d0
Apurva Kulkarni <apurvakulkarni098@gmail.com>
2024-06-16 14:29:15 +0530

    used auth in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,39 @@
 import { db } from "~/server/db";
+import { SignInButton, SignedIn, SignedOut, UserButton} from '@clerk/nextjs'
+export const dynamic = "force-dynamic";
 
-export const dynamic = "force-dynamic"
-
-export default async function HomePage() {
-
+async function Images(){
+  
   const images = await db.query.images.findMany({
     orderBy: (model, {desc}) => desc(model.id)
   });
+
+  return(
+    <div className="flex flex-wrap gap-4">
+
+    {
+      [...images, ...images, ...images].map((image, index)=>(
+        <div key={image.id + '_' + index} className="flex w-48 flex-col">
+          <img className="h-48 object-cover" src={image.url} />
+          <p>{image.name}</p>
+        </div>
+      ))
+    }
+    </div>
+  )
+}
+
+export default async function HomePage() {
+
+  
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {
-          [...images, ...images, ...images].map((image, index)=>(
-            <div key={image.id + '_' + index} className="flex w-48 flex-col">
-              <img className="h-48 object-cover" src={image.url} />
-              <p>{image.name}</p>
-            </div>
-          ))
-        }
-      </div>
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sign in from above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
